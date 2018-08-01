359a6ad04d04c908a75ddc93516e2f210152617d
tylermcrobert <31973492+tylermcrobert@users.noreply.github.com>
2018-08-01 16:06:40 -0700

    row-reverse

--- src/containers/CaseStudy/slices/Columns/Columns.js
+++ src/containers/CaseStudy/slices/Columns/Columns.js
@@ -6,22 +6,10 @@ const Columns = (props) => {
   const data = props.data.value[0];
   const text = RichText.asText(data.text);
 
-  const colContent = data.right
-    ? (
-      <React.Fragment>
-        <p>{text}</p>
-        <img src={data.image.url} alt={data.image.alt} />
-      </React.Fragment>
-    )
-    : (
-      <React.Fragment>
-        <img src={data.image.url} alt={data.image.alt} />
-        <p>{text}</p>
-      </React.Fragment>
-    );
   return (
-    <div className="caseStudy__column">
-      {colContent}
+    <div className={`caseStudy__column ${!data.right ? '' : 'caseStudy__column--right'}`}>
+      <img src={data.image.url} alt={data.image.alt} />
+      <p>{text}</p>
     </div>
   );
 };
