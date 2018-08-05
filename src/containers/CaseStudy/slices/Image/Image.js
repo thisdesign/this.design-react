1873dfa8cc04635550f7d62b816e6ff9edb5b4d6
Tyler <tyler.mcrobert@gmail.com>
2018-08-05 01:49:31 -0700

    Cleaned up layout

--- src/containers/CaseStudy/slices/Image/Image.js
+++ src/containers/CaseStudy/slices/Image/Image.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Image = (props) => {
   const src = props.data.value[0].file.url;
   return (
-    <img src={src} width="100%" />
+    <div className="casestudy__image">
+      <img src={src} width="100%" />
+    </div>
   );
 };
 
--- src/containers/CaseStudy/slices/Text/Text.js
+++ src/containers/CaseStudy/slices/Text/Text.js
@@ -2,6 +2,10 @@ import { RichText } from 'prismic-reactjs';
 import React from 'react';
 import './Text.css';
 
-const Text = props => <div className="grid"><div className="casestudy__text">{RichText.render(props.data.value)}</div></div>;
+const Text = props => (
+  <div className="grid">
+    <div className="casestudy__text">{RichText.render(props.data.value)}</div>
+  </div>
+);
 
 export default Text;
