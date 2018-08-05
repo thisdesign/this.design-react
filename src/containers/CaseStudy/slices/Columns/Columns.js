6294457baeb2b0ec1062c807e3bc5fe9a4136807
Tyler <tyler.mcrobert@gmail.com>
2018-08-05 01:27:47 -0700

    Reverted to flexbox

--- src/containers/CaseStudy/slices/Columns/Columns.js
+++ src/containers/CaseStudy/slices/Columns/Columns.js
@@ -6,6 +6,7 @@ const Columns = (props) => {
   const data = props.data.value[0];
   const text = RichText.render(data.text);
   const imageIsRight = data.right != null;
+  const imageIsLarge = data.layout !== '-column--2of3';
 
   const columnItems = [
     <div className="caseStudy__colBlock__col caseStudy__colBlock__col--media">
@@ -18,10 +19,14 @@ const Columns = (props) => {
 
   return (
     <div
-      className={`caseStudy__colBlock grid ${!data.right
+      className={`caseStudy__colBlock grid ${!imageIsRight
           ? ''
           : 'caseStudy__colBlock--right'
-        }`}
+        }
+        ${!imageIsLarge
+            ? ''
+            : 'caseStudy__colBlock--largeImage'
+          }`}
     >
       {imageIsRight ? columnItems.reverse() : columnItems}
     </div>
