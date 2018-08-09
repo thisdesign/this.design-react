ec09c4476cabd08ad44dbabfffb14a7ed1ef8cbf
tylermcrobert <31973492+tylermcrobert@users.noreply.github.com>
2018-08-09 13:19:15 -0700

    Case study cover improvements

--- src/components/CaseStudyCover/CaseStudyCover.js
+++ src/components/CaseStudyCover/CaseStudyCover.js
@@ -6,7 +6,9 @@ import './imagePositions.css';
 const CaseStudyCover = (props) => {
   const { color } = props.data;
   const header = props.data.header[0];
-  const { image1, image2 } = header;
+  const {
+    image1, image2, video1, video2,
+  } = header;
 
   const backroundImage = {
     backgroundImage: image1.url
@@ -35,9 +37,18 @@ const CaseStudyCover = (props) => {
           alt={image2.alt}
           key={image2.url}
         />
+        <video
+          autoPlay
+          loop
+          muted
+          key={video2.url}
+          className={`casestudy__splash__fr ${header.layout2}`}
+        >
+          <source src={video2.url} type="video/mp4" />
+        </video>
         <div className="casestudy__splash__bg -cover" style={backroundImage}>
-          <video autoPlay loop muted key={header.video1.url}>
-            <source src={header.video1.url} type="video/mp4" />
+          <video autoPlay loop muted key={video1.url}>
+            <source src={video1.url} type="video/mp4" />
           </video>
         </div>
       </div>
