e6158a0a11ada093330824b9c810419c2878031c
tylermcrobert <31973492+tylermcrobert@users.noreply.github.com>
2018-08-10 11:00:05 -0700

    Loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Loading.css';
 
 export default function Loading() {
   return (
-    <img
-      src="https://prismic-io.s3.amazonaws.com/thisstaging%2F802faa64-bf5a-4db5-9d56-47f802a0980c_thisloader.gif"
-      alt="loading"
-    />
+    <div className="loader">
+      <img
+        className="loader__icon"
+        src="https://prismic-io.s3.amazonaws.com/thisstaging%2F802faa64-bf5a-4db5-9d56-47f802a0980c_thisloader.gif"
+        alt="loading"
+      />
+    </div>
   );
 }
