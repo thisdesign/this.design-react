48b24723a11882021d681ad840e6c57abd774a0b
tylermcrobert <31973492+tylermcrobert@users.noreply.github.com>
2018-10-04 11:00:43 -0700

    Layout improvements

--- src/containers/CaseStudy/slices/Image/Image.js
+++ src/containers/CaseStudy/slices/Image/Image.js
@@ -4,9 +4,12 @@ import './Image.css';
 const Image = (props) => {
   const image = props.data.image || props.data.file; // v2 vs v1
   const imageIsFullScreen = props.data.layout === 'fullscreen';
+
   const largeImage = image.size_2560;
+
   const { title } = props;
   let { url } = image;
+  const { offset } = props.data;
 
   const classes = [
     'caseStudy__image',
@@ -18,6 +21,7 @@ const Image = (props) => {
   if (imageIsFullScreen && largeImage) {
     ({ url } = largeImage);
   }
+
   return (
     <div className={classes.join(' ')} >
       <picture>
@@ -25,7 +29,7 @@ const Image = (props) => {
           image.size_760 &&
             <source srcSet={image.size_760.url} media="(max-width: 600px)" />
         }
-        <img src={url} alt={title} />
+        <img src={url} alt={title} style={{ marginLeft: `${offset}%` }} />
       </picture>
     </div>
   );
