19775a04982d597fdb903d1121d95bd14d53b8d0
tylermcrobert <31973492+tylermcrobert@users.noreply.github.com>
2018-10-25 13:55:10 -0700

    Only display homepage when when video loaded

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,27 +1,46 @@
-import React from 'react';
-import './Homepage.css';
+import Loading from 'components/Loading/Loading';
+import React, { Component } from 'react';
 import isMobile from '../../util/isMobile';
+import './Homepage.css';
 
-const Homepage = (props) => {
-  const { data } = props.data;
-  const { image } = data;
-  const videoURLs = data.video_group.map(vid => vid.link.url);
-  const randomUrl = videoURLs[Math.floor(Math.random() * videoURLs.length)];
+class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.video = React.createRef();
+  }
 
-  return (
-    <div className="homepage">
-      <div className="homepage__inner">
+  state = {
+    videoLoaded: false,
+  }
 
-        {!isMobile()
-          ? (
-            <video autoPlay loop muted className="homepage__inner__video">
-              <source src={randomUrl} type="video/mp4" />
-            </video>
-          ) : <img className="homepage__inner__image" src={image.url} alt={image.alt} />
-        }
+  componentDidMount() {
+    this.video.current.onloadeddata = () => {
+      this.setState({ videoLoaded: true });
+    };
+  }
+  render() {
+    const { data } = this.props.data;
+    const { image } = data;
+    const videoURLs = data.video_group.map(vid => vid.link.url);
+    const randomUrl = videoURLs[Math.floor(Math.random() * videoURLs.length)];
+    const { videoLoaded } = this.state;
+
+    return (
+      <div className="homepage">
+        {!videoLoaded && <Loading />}
+        <div className="homepage__inner">
+          {!isMobile()
+            ? (
+              <video autoPlay loop muted className="homepage__inner__video" ref={this.video}>
+                <source src={randomUrl} type="video/mp4" />
+              </video>
+            ) : <img className="homepage__inner__image" src={image.url} alt={image.alt} />
+          }
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 export default Homepage;
