fe6a7c9aba8d1e9666258bf5a3a25f56dc90e123
Tyler <tyler.mcrobert@gmail.com>
2018-10-27 17:55:01 -0700

    Gallery inchworm animation

--- src/containers/CaseStudy/slices/Gallery/GalleryIndicators/GalleryIndicators.js
+++ src/containers/CaseStudy/slices/Gallery/GalleryIndicators/GalleryIndicators.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
 const GalleryIndicators = (props) => {
-  const indicators = props.images.map((img, index) => {
-    const className = 'caseStudy__gallery__indicator';
-    const indicatorClasses = (index === props.currentImageIndex)
-      ? `${className} ${className}--active`
-      : className;
+  const indicators = props.images.map((img, index) => (
+    <div
+      className="caseStudy__gallery__indicator"
+      key={img.url}
+      onClick={() => props.goToImage(index)}
+    />
+  ));
 
-    return (
-      <div
-        className={indicatorClasses}
-        key={img.url}
-        onClick={() => props.goToImage(index)}
-      />
-    );
-  });
+  const currentIndicator = (
+    <div
+      className="caseStudy__gallery__indicator caseStudy__gallery__indicator--current"
+      style={{ transform: `translate3d(${props.currentImageIndex * 100}%, 0, 0)` }}
+    />
+  );
+
   return (
     <div className="caseStudy__gallery__indicators">
-      {indicators}
+      <div className="caseStudy__gallery__indicators__inner">
+        {currentIndicator}
+        {indicators}
+      </div>
     </div>
   );
 };
