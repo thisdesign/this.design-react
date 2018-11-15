715e14e3b1d33d559036fa4a3ec08e0d6d0dfb90
tylermcrobert <tyler.mcrobert@gmail.com>
2018-11-15 14:49:34 -0800

    Fixed image component

--- src/containers/CaseStudy/slices/Image/Image.js
+++ src/containers/CaseStudy/slices/Image/Image.js
@@ -3,16 +3,19 @@ import './Image.css';
 
 const Image = (props) => {
   const isV2Slice = props.data.primary !== undefined;
+
+  const data = isV2Slice // get based on v1 vs v2 api
+    ? props.data.primary
+    : props.data.value[0];
+
   const image = isV2Slice // get based on v1 vs v2 api
-    ? props.data.primary.image
-    : props.data.value[0].file;
-  const imageIsFullScreen = props.data.layout === 'fullscreen';
+    ? data.image
+    : data.file;
 
-  const largeImage = image.size_2560;
-
-  const { title } = props;
+  const imageIsFullScreen = data.layout === 'fullscreen';
+  const largeImageData = image.size_2560;
   let { url } = image;
-  const { offset } = props.data;
+  const { offset } = data;
 
   const classes = [
     'caseStudy__image',
@@ -21,8 +24,8 @@ const Image = (props) => {
       : '-centered -wrap',
   ];
 
-  if (imageIsFullScreen && largeImage) {
-    ({ url } = largeImage);
+  if (imageIsFullScreen && largeImageData) {
+    ({ url } = largeImageData);
   }
 
   return (
@@ -32,7 +35,7 @@ const Image = (props) => {
           image.size_760 &&
             <source srcSet={image.size_760.url} media="(max-width: 600px)" />
         }
-        <img src={url} alt={title} style={{ marginLeft: `${offset}%` }} />
+        <img src={url} alt={props.title} style={{ marginLeft: `${offset}%` }} />
       </picture>
     </div>
   );
