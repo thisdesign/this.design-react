d503424f2d62bfb1c2eb6961d021395b0a501faa
tylermcrobert <tyler.mcrobert@gmail.com>
2018-12-18 13:12:15 -0800

    Fixed Preview

--- src/containers/PrismicApp/Preview/Preview.js
+++ src/containers/PrismicApp/Preview/Preview.js
@@ -1,19 +1,26 @@
 import React from 'react';
-
-import Cookies from 'js-cookie';
 import qs from 'qs';
 import Prismic from 'prismic-javascript';
+import Cookies from 'js-cookie';
 import PrismicConfig from '../config/prismic-configuration';
 
 const PREVIEW_EXPIRES = 1 / 48; // 30 minutes
 
 export default class Preview extends React.Component {
-  componentWillReceiveProps(props) {
-    const params = qs.parse(props.location.search.slice(1));
-    props.prismicCtx.api.previewSession(params.token, PrismicConfig.linkResolver, '/').then((url) => {
-      Cookies.set(Prismic.previewCookie, params.token, { expires: PREVIEW_EXPIRES });
-      window.location.replace(url);
-    });
+  componentWillMount() {
+    this.preview(this.props);
+  }
+
+  preview(props) {
+    const params = qs.parse(this.props.location.search.slice(1));
+    Prismic.getApi(PrismicConfig.apiEndpoint)
+      .then((api) => {
+        api.previewSession(params.token, PrismicConfig.linkResolver, '/')
+          .then((url) => {
+            Cookies.set(Prismic.previewCookie, params.token, { expires: PREVIEW_EXPIRES });
+            props.history.push(url);
+          });
+      });
   }
 
   render() {
