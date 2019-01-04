7c752e871c120c51e49f021991d34be2b86c24db
tylermcrobert <tyler.mcrobert@gmail.com>
2019-01-04 13:59:36 -0800

    Implimented rAF

--- src/components/Parallax/libs/ParallaxController.js
+++ src/components/Parallax/libs/ParallaxController.js
@@ -2,11 +2,14 @@ export default class ParallaxController {
   constructor({ container, el }) {
     this.container = container;
     this.el = el;
+    this.scrollTop = 0;
+    this.ticking = false;
   }
+
   addListener = () => {
     this.container.addEventListener(
       'scroll',
-      this.setOffset,
+      this.handleScroll,
       { passive: true },
     );
   }
@@ -15,11 +18,21 @@ export default class ParallaxController {
     this.container = container;
   }
 
+  handleScroll = () => {
+    this.scrollTop = this.container.scrollTop;
+
+    if (!this.ticking) {
+      this.ticking = true;
+      window.requestAnimationFrame(this.setOffset);
+    }
+  }
+
   setOffset = () => {
     const bounds = this.getBounds();
     const idk = ((bounds.top + bounds.height) / 2) - (window.innerHeight / 2);
     const offset = (-idk / 4);
     this.el.style.transform = `translate3d(0,${offset}px, 0)`;
+    this.ticking = false;
   }
 
   init = () => {
