a1f6665f806e4379be76357ca648edb83da7411c
tylermcrobert <tyler.mcrobert@gmail.com>
2019-01-11 16:52:08 -0800

    Fixed viewport units on mobileSafari

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PrismicApp from './containers/PrismicApp/PrismicApp';
 
+require('viewport-units-buggyfill').init();
+const hacks = require('viewport-units-buggyfill/viewport-units-buggyfill.hacks');
+require('viewport-units-buggyfill').init({
+  hacks,
+});
+
 ReactDOM.render(
   <PrismicApp />,
   document.getElementById('root'),
