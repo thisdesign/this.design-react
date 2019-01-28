ec1a9fe49197c5339a055bb86af62ddffb6db7b4
tylermcrobert <tyler.mcrobert@gmail.com>
2019-01-28 11:09:23 -0800

    Styled image

--- src/components/ResponsiveImage/ResponsiveImage.js
+++ src/components/ResponsiveImage/ResponsiveImage.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ResponsiveImage = ({ mobileSrc, src, alt }) => (
+const ResponsiveImage = ({ mobile, children, desktop }) => (
   <picture>
-    { mobileSrc &&
-      <source srcSet={mobileSrc} media="(max-width: 600px)" />
+    { mobile &&
+      <source srcSet={mobile} media="(max-width: 600px)" />
     }
-    <img {...{ src }} alt={alt} />
+    { desktop &&
+      <source srcSet={desktop} media="(min-width: 1440px)" />
+    }
+    {children}
   </picture>
 );
 
+ResponsiveImage.defaultProps = {
+  mobile: null,
+  desktop: null,
+};
+
 ResponsiveImage.propTypes = {
-  mobileSrc: PropTypes.string.isRequired,
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
+  mobile: PropTypes.string,
+  desktop: PropTypes.string,
+  children: PropTypes.node.isRequired,
 };
 
 export default ResponsiveImage;
--- src/containers/CaseStudy/slices/Image/Image.js
+++ src/containers/CaseStudy/slices/Image/Image.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ResponsiveImage from 'components/ResponsiveImage/ResponsiveImage';
+import { CsContext } from 'containers/CaseStudy/CaseStudy';
+import PropTypes from 'prop-types';
 import './Image.scss';
+import Styled from './styled';
 
 const Image = ({
-  isFullScreen, offset, url, title, mobileUrl,
+  src,
+  responsiveUrls,
+  fullscreen,
+  offset,
 }) => {
-  const classes = [
-    'caseStudy__image',
-    isFullScreen
-      ? 'caseStudy__image--fullscreen'
-      : '-centered -wrap',
-  ];
-
+  const { alt } = useContext(CsContext);
   return (
-    <div className={classes.join(' ')} >
-      <picture>
-        { mobileUrl &&
-          <source srcSet={mobileUrl} media="(max-width: 600px)" />
-        }
-        <img src={url} alt={title} style={{ marginLeft: `${offset}%` }} />
-      </picture>
-    </div>
+    <Styled.ImageWrapper {...{ fullscreen }}>
+      <ResponsiveImage
+        mobile={responsiveUrls.mobile}
+        desktop={responsiveUrls.desktop}
+      >
+        <Styled.Img
+          src={
+            (fullscreen && responsiveUrls.fullWidth)
+              ? responsiveUrls.fullWidth
+              : src
+          }
+          alt={alt}
+          offset={offset}
+        />
+      </ResponsiveImage>
+    </Styled.ImageWrapper>
   );
+};
+
+Image.defaultProps = {
+  offset: 0,
+  responsiveUrls: {
+    mobile: null,
+    fullWidth: null,
+    desktop: null,
+  },
+};
+
+Image.propTypes = {
+  fullscreen: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  offset: PropTypes.number,
+  responsiveUrls: PropTypes.shape({
+    mobile: PropTypes.string,
+    fullWidth: PropTypes.string,
+    desktop: PropTypes.string,
+  }),
 };
 
 export default React.memo(Image);
