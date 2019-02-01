d63d391ca72e510ebe4fb820e4554c7bbce3c5ea
tylermcrobert <tyler.mcrobert@gmail.com>
2019-02-01 14:13:17 -0800

    converted WaypointAnim to hooks

--- src/components/WaypointAnim/WaypointAnim.js
+++ src/components/WaypointAnim/WaypointAnim.js
@@ -1,48 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Waypoint from 'react-waypoint';
 import './WaypointAnim.scss';
 
-export default class WaypointAnim extends React.Component {
-  state = {
-    visible: false,
-  }
+export default function WaypointAnim({
+  name,
+  className,
+  children,
+  disabled,
+}) {
+  const [visible, setVisible] = useState(false);
+  const classNames = [
+    className,
+    `-waypointAnim--${name}`,
+    visible ? '-enabled' : '',
+  ].join(' ');
 
-  render() {
-    const {
-      name,
-      className,
-      children,
-      disabled,
-    } = this.props;
-
-    const classNames = [
-      className,
-      `-waypointAnim--${name}`,
-      this.state.visible ? '-enabled' : '',
-    ].join(' ');
-
-    return (
-      <Waypoint
-        onEnter={() => this.setState({ visible: true })}
-        onLeave={() => this.setState({ visible: false })}
-        topOffset="0px"
-        bottomOffset="0px"
-      >
-        <div className={!disabled ? classNames : className}>
-          {children}
-        </div>
-      </Waypoint>
-    );
-  }
+  return (
+    <Waypoint
+      onEnter={() => setVisible(true)}
+      onLeave={() => setVisible(false)}
+      topOffset="0px"
+      bottomOffset="0px"
+    >
+      <div className={!disabled ? classNames : className}>
+        {children}
+      </div>
+    </Waypoint>
+  );
 }
 
 WaypointAnim.defaultProps = {
   name: 'fadeIn',
   className: '',
+  disabled: false,
 };
 WaypointAnim.propTypes = {
   children: PropTypes.node.isRequired,
   name: PropTypes.string,
   className: PropTypes.string,
+  disabled: PropTypes.bool,
 };
