a08936841e4f432d99371c461c13f44cb3da0d52
tylermcrobert <tyler.mcrobert@gmail.com>
2019-02-06 10:14:45 -0800

    Added useInterval

---
+++ src/hooks/useInterval.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+export default function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  // Set up the interval.
+  useEffect(() => { //eslint-disable-line
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
