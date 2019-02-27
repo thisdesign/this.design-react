c13ced7e919833b8cfe93efb5a605e8a5bcc3a54
tylermcrobert <tyler.mcrobert@gmail.com>
2019-02-27 11:41:25 -0800

    useVideoStatus

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -1,18 +1,15 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
 import Loading from 'components/Loading/Loading';
 import PropTypes from 'prop-types';
 import HomepageWrapper from './HomepageWrapper';
+
+import useVideoStatus from './hooks/useVideoStatus';
 import Styled from './styled';
 import Posed from './posed';
 
+
 function Homepage({ openingFromHome, randomUrl }) {
-  const [videoLoaded, setVideoLoaded] = useState(false);
-  const ref = useRef();
-
-  useEffect(() => {
-    ref.current.onloadeddata = () => setVideoLoaded(true);
-  }, []);
-
+  const { videoLoaded, ref } = useVideoStatus();
   return (
     <Posed.Homepage pose={openingFromHome ? 'animating' : 'normal'}>
       {!videoLoaded && <Loading />}
