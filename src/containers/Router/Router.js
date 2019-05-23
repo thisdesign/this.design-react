1853bca36866a30b25d7421c50180c84d7403551
tylermcrobert <tyler.mcrobert@gmail.com>
2019-05-23 12:27:14 -0700

    Router fix

--- src/containers/Router/Router.js
+++ src/containers/Router/Router.js
@@ -1,23 +1,26 @@
 import React, { useContext } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, matchPath } from "react-router-dom";
 import { ApiDataCtx } from "../App/App";
 import Layout from "../Layout/Layout";
 
 function Router() {
-  const { contextUids, caseStudies } = useContext(ApiDataCtx);
-  const caseStudyUids = caseStudies.map(cs => cs.uid);
+  const { caseStudies } = useContext(ApiDataCtx);
   return (
     <BrowserRouter>
       <Switch>
         <Route
           exact
-          path="/work/:uid"
+          path={caseStudies.map(cs => `/work/${cs.uid}`)}
           render={({ match }) => (
-            <Layout view="root" isWorkView pathUid={match.params.uid} />
+            <Layout
+              view="root"
+              isWorkView
+              pathUid={matchPath(match.url, { path: "/work/:uid" }).params.uid}
+            />
           )}
         />
-        <Route path="/work/" render={() => <Layout view="work" />} />
-        <Route path="/about/" render={() => <Layout view="about" />} />
+        <Route exact path="/work/" render={() => <Layout view="work" />} />
+        <Route exact path="/about/" render={() => <Layout view="about" />} />
         <Route path="/" render={() => <Layout view="root" />} />
       </Switch>
     </BrowserRouter>
