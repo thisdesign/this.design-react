82e40656c5dd4d7c53d74da771bb4edad1ca17cd
tylermcrobert <tyler.mcrobert@gmail.com>
2019-06-07 09:45:50 -0700

    Wrap fullscreen option

--- src/components/Styled/index.js
+++ src/components/Styled/index.js
@@ -1,9 +1,3 @@
-import styled from 'styled-components/macro'
+import Wrap from './Wrap'
 
-export const Wrap = styled.div`
-  max-width: 1500px;
-  width: 80%;
-  margin: 0px auto;
-`
-
-export default { Wrap }
+export { Wrap }
