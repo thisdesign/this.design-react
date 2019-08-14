460605d07591d115110f5b020c364e1c96837b56
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-14 11:05:30 -0700

    react-snap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM, { hydrate, render } from 'react-dom'
 import App from './containers/App/App'
+
+const rootElement = document.getElementById('root')
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement)
+} else {
+  render(<App />, rootElement)
+}
 
 require('viewport-units-buggyfill').init()
 const hacks = require('viewport-units-buggyfill/viewport-units-buggyfill.hacks')
