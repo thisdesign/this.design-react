8e278cfe559f1fa3f3430ed0b6493d33b03aaff6
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-16 10:45:25 -0700

    Sitemap

---
+++ sitemap.js
@@ -0,0 +1,31 @@
+const Prismic = require('prismic-javascript')
+const fs = require('fs')
+const { createSitemap } = require('sitemap')
+
+console.log('building sitemap...')
+
+async function getContextUids() {
+  const api = await Prismic.api('https://thisstaging.prismic.io/api/v2')
+  const contextUids = await api
+    .getByUID('context', 'home')
+    .then(doc => doc)
+    .then(res => res.data.case_study_list.map(cs => cs.case_study_item.uid))
+  return contextUids
+}
+
+async function buildSitemap() {
+  const UIDS = await getContextUids()
+  const sitemap = createSitemap({
+    hostname: 'http://this.design',
+    urls: [
+      { url: '/work/' },
+      { url: '/about/' },
+      { url: '/' },
+      ...UIDS.map(uid => ({ url: `/${uid}/` })),
+    ],
+  })
+  const xml = sitemap.toXML()
+  fs.writeFile('public/sitemap.xml', xml)
+}
+
+buildSitemap()
