65cd4813a3b32e46bc4724026a7910c3dee1ea72
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-28 09:35:51 -0700

    Use static data

--- src/structure/DataProvider.js
+++ src/structure/DataProvider.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect, createContext, useContext } from 'react'
 import getPrismicData from 'data/getPrismicData'
+import APP_DATA from 'data/APP_DATA'
 
 const AppDataCtx = createContext()
 
-function useFetchData(cond = true) {
+function useFetchData(shouldFetch = true) {
   const [siteData, setSiteData] = useState()
 
   useEffect(() => {
-    if (cond) getPrismicData().then(res => setSiteData(res))
-  }, [cond])
+    if (shouldFetch) getPrismicData().then(res => setSiteData(res))
+  }, [shouldFetch])
 
   return siteData
 }
 
 function DataProvider({ children }) {
-  const SHOULD_FETCH = true
+  const SHOULD_FETCH = false
+
+  const staticDta = APP_DATA.data
   const fetchedData = useFetchData(SHOULD_FETCH)
-  const siteData = SHOULD_FETCH ? fetchedData : null
+
+  const siteData = SHOULD_FETCH ? fetchedData : staticDta
 
   return (
     <AppDataCtx.Provider
