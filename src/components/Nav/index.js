950be2bd5d0c5f4162b5241758fb9b4ee02e13b5
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-28 11:38:23 -0700

    Close asides

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { LayoutCtx } from 'structure/Layout'
 import Styled from './Styled'
 
 function Nav() {
+  const { view } = useContext(LayoutCtx)
+  const isRoot = view === 'root'
+
   return (
     <Styled.Nav as="nav" padTop>
       <Styled.Wrapper>
         <li>
-          <Link to="/work">Work</Link>
+          <Link to={isRoot ? '/work' : '/'}>{isRoot ? 'work' : 'close'}</Link>
         </li>
         <li>
-          <Link to="/about">about</Link>
+          <Link to={isRoot ? '/about' : '/'}>{isRoot ? 'about' : 'close'}</Link>
         </li>
       </Styled.Wrapper>
     </Styled.Nav>
--- src/structure/Layout/index.js
+++ src/structure/Layout/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import { useData } from 'structure/DataProvider'
 import GlobalStyle from 'style/GlobalStyle'
 import Nav from 'components/Nav'
@@ -9,43 +9,47 @@ import Wrap from 'components/Wrap'
 import Section from 'components/Section'
 import Styled from './Styled'
 
+export const LayoutCtx = createContext()
+
 function Layout({ view }) {
   const data = useData()
 
   return (
-    <ThemeProvider theme={theme}>
-      <>
-        <GlobalStyle />
-        <Nav />
-        <ThemeProvider theme={{ view }}>
-          <>
-            <Styled.View.Root as="main">
-              <Section>
-                <Wrap>
-                  <h3>root</h3>
-                </Wrap>
-              </Section>
-            </Styled.View.Root>
-            <Styled.View.About>
-              <Section>
-                <Wrap>
-                  <h3>About</h3>
-                </Wrap>
-              </Section>
-            </Styled.View.About>
-            <Styled.View.Work>
-              <Section>
-                <Wrap>
-                  {data.allCaseStudies.map(item => (
-                    <div key={item.uid}>{item.uid}</div>
-                  ))}
-                </Wrap>
-              </Section>
-            </Styled.View.Work>
-          </>
-        </ThemeProvider>
-      </>
-    </ThemeProvider>
+    <LayoutCtx.Provider value={{ view }}>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <Nav />
+          <ThemeProvider theme={{ view }}>
+            <>
+              <Styled.View.Root as="main">
+                <Section>
+                  <Wrap>
+                    <h3>root</h3>
+                  </Wrap>
+                </Section>
+              </Styled.View.Root>
+              <Styled.View.About>
+                <Section>
+                  <Wrap>
+                    <h3>About</h3>
+                  </Wrap>
+                </Section>
+              </Styled.View.About>
+              <Styled.View.Work>
+                <Section>
+                  <Wrap>
+                    {data.allCaseStudies.map(item => (
+                      <div key={item.uid}>{item.uid}</div>
+                    ))}
+                  </Wrap>
+                </Section>
+              </Styled.View.Work>
+            </>
+          </ThemeProvider>
+        </>
+      </ThemeProvider>
+    </LayoutCtx.Provider>
   )
 }
 
