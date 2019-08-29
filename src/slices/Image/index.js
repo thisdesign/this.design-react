83c6d73ba65e1f3db646af2622ffb2fb2810399c
tylermcrobert <tyler.mcrobert@gmail.com>
2019-08-29 12:15:19 -0700

    Image slice updates

--- src/slices/Image/index.js
+++ src/slices/Image/index.js
@@ -1,18 +1,24 @@
 import React, { useContext } from 'react'
-import { ImgShell } from 'components/LazyImg'
+import LazyImg from 'components/LazyImg'
 import { CaseStudyDataCtx } from 'structure/CaseStudy'
 import formatAlt from 'util/formatAlt'
 import PropTypes from 'prop-types'
+import Wrap from 'components/Wrap'
 
-function Image({ aspect, alt, src }) {
-  const img = <ImgShell {...{ aspect, alt, src }} />
-  return <div>{img}</div>
+function Image({ aspect, alt, src, fullScreen }) {
+  const img = <LazyImg {...{ aspect, alt, src }} />
+  return fullScreen ? img : <Wrap>{img}</Wrap>
+}
+
+Image.defaultProps = {
+  fullScreen: false,
 }
 
 Image.propTypes = {
   aspect: PropTypes.number.isRequired,
   alt: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
+  fullScreen: PropTypes.bool,
 }
 
 /* eslint-disable react/prop-types  */
@@ -21,10 +27,18 @@ Image.CSDataWrapper = function CSDataWrapper({ data }) {
   const {
     data: { title },
   } = useContext(CaseStudyDataCtx)
-  const { image } = data.primary
+  const { image, position, offset, layout } = data.primary
   const { width, height } = image.dimensions
+
+  console.log({ position, offset, layout })
+
   return (
-    <Image src={image.url} alt={formatAlt(title)} aspect={height / width} />
+    <Image
+      src={image.url}
+      fullScreen={layout === 'fullscreen'}
+      alt={formatAlt(title)}
+      aspect={height / width}
+    />
   )
 }
 
