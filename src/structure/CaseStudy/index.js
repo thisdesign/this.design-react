11292d0bfa1a4fc135e210951b8e96d035523fe6
Tyler <tyler.mcrobert@gmail.com>
2019-09-02 12:18:23 -0700

    CaseStudyWrapper

--- src/structure/CaseStudy/index.js
+++ src/structure/CaseStudy/index.js
@@ -17,19 +17,27 @@ const CaseStudy = ({ uid }) => {
   const { hoveredCsUID, mainRef } = useContext(LayoutCtx)
   const { transitionName } = useContext(TransitionCtx)
   const isTransitioningFromWork = transitionName === 'FROM_WORK'
-  const csData = useCsData(uid)
 
   if (isTransitioningFromWork) {
     mainRef.current.scrollTop = 0
   }
 
   return (
+    <CaseStudyWrapper uid={uid}>
+      <Hero uid={hoveredCsUID || uid} />
+      {!isTransitioningFromWork && <Slices uid={uid} />}
+    </CaseStudyWrapper>
+  )
+}
+
+function CaseStudyWrapper({ children, uid }) {
+  const csData = useCsData(uid)
+  return (
     <Styled.CaseStudy
       bg={csData.data.background_color}
       text={csData.data.text_color}
     >
-      <Hero uid={hoveredCsUID || uid} />
-      {!isTransitioningFromWork && <Slices uid={uid} />}
+      {children}
     </Styled.CaseStudy>
   )
 }
