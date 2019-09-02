b6b168b45797eb4dc3c5f92578fe6dc6fe117db2
Tyler <tyler.mcrobert@gmail.com>
2019-09-01 23:56:53 -0700

    Nav color

--- src/structure/Nav/Styled.js
+++ src/structure/Nav/Styled.js
@@ -5,6 +5,7 @@ const Nav = styled(Wrap.Nav)`
   position: fixed;
   z-index: 20;
   width: 100%;
+  color: ${props => (props.theme.isNavWhite ? 'white' : 'black')};
 `
 
 const Wrapper = styled.ul`
--- src/structure/Nav/index.js
+++ src/structure/Nav/index.js
@@ -1,28 +1,35 @@
 import React, { useContext } from 'react'
 import { LayoutCtx } from 'structure/Layout'
 import TransitionLink from 'components/TransitionLink'
+import { ThemeProvider } from 'styled-components'
 import Styled from './Styled'
 
 function Nav() {
   const { view, currentCsUid } = useContext(LayoutCtx)
   const isRoot = view === 'root'
+  const isAbout = view === 'about'
   const closeRoute = currentCsUid ? `/work/${currentCsUid}` : '/'
 
+  const csIsScrolled = false // change
+  const isNavWhite = isAbout || (isRoot && !csIsScrolled)
+
   return (
-    <Styled.Nav as="nav" padTop>
-      <Styled.Wrapper>
-        <li>
-          <TransitionLink to={isRoot ? '/work' : closeRoute}>
-            {isRoot ? 'work' : 'close'}
-          </TransitionLink>
-        </li>
-        <li>
-          <TransitionLink to={isRoot ? '/about' : closeRoute}>
-            {isRoot ? 'about' : 'close'}
-          </TransitionLink>
-        </li>
-      </Styled.Wrapper>
-    </Styled.Nav>
+    <ThemeProvider theme={{ isNavWhite }}>
+      <Styled.Nav as="nav" padTop>
+        <Styled.Wrapper>
+          <li>
+            <TransitionLink to={isRoot ? '/work' : closeRoute}>
+              {isRoot ? 'work' : 'close'}
+            </TransitionLink>
+          </li>
+          <li>
+            <TransitionLink to={isRoot ? '/about' : closeRoute}>
+              {isRoot ? 'about' : 'close'}
+            </TransitionLink>
+          </li>
+        </Styled.Wrapper>
+      </Styled.Nav>
+    </ThemeProvider>
   )
 }
 
