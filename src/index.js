4f94e9db50dae19a8aa41aabba6a785d00aa1164
tylermcrobert <tyler.mcrobert@gmail.com>
2019-09-03 10:07:36 -0700

    Whydidyourender

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { hydrate, render } from 'react-dom'
 import Layout from './structure'
 
+if (process.env.NODE_ENV !== 'production') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render')
+  whyDidYouRender(React)
+}
+
 const rootElement = document.getElementById('root')
 
 if (rootElement.hasChildNodes()) {
