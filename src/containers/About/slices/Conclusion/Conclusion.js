f2373d1dba3d2a1875d530fdc0304c47885a5134
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-26 10:25:23 -0800

    1% planet added

--- src/containers/About/slices/Conclusion/Conclusion.js
+++ src/containers/About/slices/Conclusion/Conclusion.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { RichText } from 'prismic-reactjs'
 import './Conclusion.scss'
 
+const ONE_PERCENT_LOGO =
+  'https://images.prismic.io/thisstaging/36b4d66c-2808-49c6-9bb1-a59122c7609f_One+Percent.svg'
+
 const Conclusion = props => {
   const data = props.data.primary
   return (
@@ -10,6 +13,7 @@ const Conclusion = props => {
         {RichText.render(data.large_text)}
         {RichText.render(data.contact)}
         {RichText.render(data.address)}
+        <img src={ONE_PERCENT_LOGO} />
       </div>
     </div>
   )
