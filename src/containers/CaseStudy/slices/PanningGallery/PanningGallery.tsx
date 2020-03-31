454ebc9dd27c9ae064ca672dcb4e1bac03fd1c84
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-31 14:18:12 -0700

    get index change on hover

--- src/containers/CaseStudy/slices/PanningGallery/PanningGallery.tsx
+++ src/containers/CaseStudy/slices/PanningGallery/PanningGallery.tsx
@@ -17,6 +17,17 @@ const PanningGallery: React.FC<{ data: PanningGallery }> = ({ data }) => {
   const galleryRef = useRef()
   const isDoubleGrouped = data.primary.cell_grouping === true
 
+  const position = useRef({
+    furthestLeft: null,
+    furthestRight: null,
+  })
+
+  const getIndexChange = (mouseX): -1 | 0 | 1 => {
+    if (position.current.furthestLeft > mouseX) return -1
+    if (position.current.furthestRight < mouseX) return 1
+    return 0
+  }
+
   useEffect(() => {
     if (galleryRef.current) {
       const flkty = new Flickity(galleryRef.current, {
@@ -27,32 +38,43 @@ const PanningGallery: React.FC<{ data: PanningGallery }> = ({ data }) => {
         groupCells: isDoubleGrouped ? 2 : 1,
       })
 
-      flkty.on('staticClick', (event, pointer, cellElement, cellIndex) => {
-        const getIndexChange = (): -1 | 0 | 1 => {
-          const centerRects = flkty.selectedCells.map(item =>
-            item.element.getBoundingClientRect()
-          )
-          const furthestLeft = Math.min(...centerRects.map(rect => rect.x))
-          const furthestRight = Math.max(
-            ...centerRects.map(rect => rect.x + rect.width)
-          )
+      const setPosition = () => {
+        const centerRects = flkty.selectedCells.map(item =>
+          item.element.getBoundingClientRect()
+        )
 
-          if (furthestLeft > event.clientX) return -1
-          if (furthestRight < event.clientX) return 1
-          return 0
-        }
+        const furthestLeft = Math.min(...centerRects.map(rect => rect.x))
+        const furthestRight = Math.max(
+          ...centerRects.map(rect => rect.x + rect.width)
+        )
 
-        const indexChange = getIndexChange()
+        position.current = { furthestRight, furthestLeft }
+      }
+
+      setPosition()
+
+      flkty.on('change', setPosition)
+
+      flkty.on('staticClick', event => {
+        const indexChange = getIndexChange(event.clientX)
         flkty.select(flkty.selectedIndex + indexChange, true)
       })
     }
   }, [])
+
+  const handleMouseOver = (e: React.MouseEvent) => {
+    console.log(getIndexChange(e.clientX))
+  }
+
   return (
     <S.Wrapper ref={galleryRef} className="flexy-carousel ">
       {data.items.map(
         (item, i) =>
           item.image.url && (
-            <S.ImageWrapper width={data.primary.cell_width}>
+            <S.ImageWrapper
+              width={data.primary.cell_width}
+              onMouseMove={handleMouseOver}
+            >
               <img src={item.image.url} />
             </S.ImageWrapper>
           )
